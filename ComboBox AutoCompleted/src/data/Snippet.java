fce43984d72adafd9c9b5b32526f3524d6245a8d
aerdy <aerdy4@gmail.com>
2014-08-11 13:14:43 +0700

    ZK Combobox
    ZK Combobox

---
+++ ComboBox AutoCompleted/src/data/Snippet.java
@@ -0,0 +1,10 @@
+package data;
+
+public class Snippet {
+	static String[] dictionary = { "Annabe","Anna","Arthdi","Putra","Bandung","Banten","Curug","Cilacap","Dadang","Dumai","Darusalam"
+		,"Elang"};
+		public static String[] getDirectory() {
+			return dictionary;
+		}
+}
+
---
+++ ComboBoxAutoComplete/src/data/StringData.java
@@ -0,0 +1,11 @@
+package data;
+
+public class StringData {
+	static String[] Data = {"Annabe","Anna","Arthdi","Putra","Bandung","Banten","Curug","Cilacap","Dadang","Dumai","Darusalam"
+		,"Elang",};
+	//kita buat method untuk memanggil array itu
+	public static String[] getData(){
+		return Data;
+	}
+}
+//setelah kita membuat class dengan data yang akan di tampilkan kita aka
